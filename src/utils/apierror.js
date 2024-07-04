4c78e2f2ad1ae867d8ec0e2664e127eea2fee670
ananya2313 <ananyaoct24@gmail.com>
2024-07-04 13:04:28 +0530

    util code discussions

---
+++ src/utils/apierror.js
@@ -0,0 +1,27 @@
+class apierror extends Error{
+
+    constructor(
+        statusCode,
+        message="Something went wrong",
+        errors=[],
+        statck=""
+    ){
+        super(message)
+        this.statusCode=statusCode
+        this.data=nullthis.message=message
+        this.success=false;
+        this.errors=errors
+
+
+        if(statck){
+            this.stack=statck
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+
+}
+
+
+export {apierror}
